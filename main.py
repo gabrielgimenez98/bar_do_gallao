d37c0765cfa5746290abca1ac3a8cbfff0dbf682
Gabriel Gimenez <gabriel.gimenez@legalbot.com.br>
2019-11-18 18:59:53 -0300

    mudancas finais do projeto

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 apresent = """
 
 |\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/|
-|                                                                |
-|                     Bar do Gallao                              | 
-|                                                                |
-|________________________________________________________________|
-|                Para fazer um drink fraco:                      |
-|                                                                |
-|                                                                |
+|                     Bar do Gallao                              |
 |                                                                |
 |________________________________________________________________|
-|                Para fazer um drink suave:                      |
-|                                                                |
+|                Preço de um drink fraco:                        |
+|                        R$ 15,00                                |
 |                                                                |
 |                                                                |
 |________________________________________________________________|
-|                Para fazer um drink forte:                      |
+|                Preço de um drink suave:                        |
+|                        R$ 20,00                                |
 |                                                                |
+|                                                                |
+|________________________________________________________________|
+|                Preço de um drink forte:                        |
+|                        R$ 25,00                                |
 |                                                                |
 |                                                                | 
 |________________________________________________________________|
-|                Para fazer um drink fraco:                      |
-|                                                                |
-|                                                                |
-|                                                                |
+|Para ser considerado um drink Cuba Libre,precisa ter:           |
+|Coca-Cola (50ml a 60ml) ou Pepsi-Cola (60ml a 70ml)             |
+|Rum (10ml a 30ml)                                               |
+|Gelo (20ml)                                                     |
 |________________________________________________________________|
-|                Para fazer um drink fraco:                      |
-|                                                                |
+|          Sera cobrado R$ 30,00 caso seja feita                 |
+|          uma mistura fora do padrao especificado               |
 |                                                                |
 |                                                                |
 |________________________________________________________________|
@@ -134,17 +133,18 @@ def defuzzy(lst_x,lst_y,lst_z):
 
 
 
-#menu foda
+#menu
 print(apresent)
 
 # lista de opcao dos refrigerantes
 refri_lst = ['Coca-Cola','Pepsi-Cola']
 
 op_refri = 0
+falta_cola = falta_rum = falta_gelo = False
 
 while op_refri != 1 or op_refri != 2:
     op_refri = int(input("""
-    Qual refrigerante tu quer usar, bro?
+    Qual refrigerante vamos usar para fazer o drink?
 
     1 - Coca-Cola
 
@@ -155,46 +155,138 @@ while op_refri != 1 or op_refri != 2:
 cola = gelo = rum = -1
 #inputs dos dados
 while cola < 0 or cola > 100:
-    cola = float(input(f'quantos ml de {refri_lst[op_refri-1]} vai ter no drink?\n'))
+    cola = float(input(f'quantos ml de {refri_lst[op_refri-1]}  vai ter no drink?(entre 0 e 100ml)\n'))
 
 while gelo < 0 or gelo > 100:
-    gelo = float(input(f'quantos ml de gelo vai ter no drink?\n'))
+    gelo = float(input(f'quantos ml de gelo vai ter no drink?(entre 0 e 100ml)\n'))
 
 while rum < 0 or rum > 100:
-    rum = float(input(f'quantos ml de rum vai ter no drink?\n'))
+    rum = float(input(f'quantos ml de rum vai ter no drink?(entre 0 e 100ml)\n'))
+
+#armazenando pertinencias de rum e gelo
+perti_rum_forte = pertinencia_rum_forte(rum)
+perti_rum_suave = pertinencia_rum_suave(rum)
+perti_rum_fraco = pertinencia_rum_fraco(rum)
+perti_gelo = pertinencia_gelo(gelo)
 
 
+
+# logica de defuzzyficar caso for coca-cola
 if op_refri == 1:
+    
+    perti_cola_forte = pertinencia_coca_forte(cola)
+    perti_cola_suave = pertinencia_coca_suave(cola)
+    perti_cola_fraco = pertinencia_coca_fraco(cola)
 
     defuzzy_suave = defuzzy([
-    pertinencia_coca_forte(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_suave(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_fraco(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo)])
+    perti_cola_forte,perti_rum_fraco,perti_gelo],
+    [perti_cola_suave,perti_rum_suave,perti_gelo],
+    [perti_cola_fraco,perti_rum_forte,perti_gelo])
 
     defuzzy_forte = defuzzy([
-    pertinencia_coca_forte(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_forte(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_suave(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo)])
+    perti_cola_forte,perti_rum_suave,perti_gelo],
+    [perti_cola_forte,perti_rum_forte,perti_gelo],
+    [perti_cola_suave,perti_rum_forte,perti_gelo])
 
     defuzzy_fraco = defuzzy([
-    pertinencia_coca_fraco(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_fraco(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_coca_suave(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo)])
+    perti_cola_fraco,perti_rum_fraco,perti_gelo],
+    [perti_cola_fraco,perti_rum_suave,perti_gelo],
+    [perti_cola_suave,perti_rum_fraco,perti_gelo])
+    
+    if not any(perti_cola_fraco,perti_cola_suave,perti_cola_forte):
+        falta_cola = True
 
+# logica de defuzzyficar caso for pepsi
 else:
+    
+    perti_cola_forte = pertinencia_pepsi_forte(cola)
+    perti_cola_suave = pertinencia_pepsi_suave(cola)
+    perti_cola_fraco = pertinencia_pepsi_fraco(cola)
+    
     defuzzy_suave = defuzzy([
-    pertinencia_pepsi_forte(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_suave(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_fraco(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo)])
+    perti_cola_forte,perti_rum_fraco,perti_gelo],
+    [perti_cola_suave,perti_rum_suave,perti_gelo],
+    [perti_cola_fraco,perti_rum_forte,perti_gelo])
 
     defuzzy_forte = defuzzy([
-    pertinencia_pepsi_forte(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_forte(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_suave(cola),pertinencia_rum_forte(rum),pertinencia_gelo(gelo)])
+    perti_cola_forte,perti_rum_suave,perti_gelo],
+    [perti_cola_forte,perti_rum_forte,perti_gelo],
+    [perti_cola_suave,perti_rum_forte,perti_gelo])
 
     defuzzy_fraco = defuzzy([
-    pertinencia_pepsi_fraco(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_fraco(cola),pertinencia_rum_suave(rum),pertinencia_gelo(gelo),
-    pertinencia_pepsi_suave(cola),pertinencia_rum_fraco(rum),pertinencia_gelo(gelo)])
+    perti_cola_fraco,perti_rum_fraco,perti_gelo],
+    [perti_cola_fraco,perti_rum_suave,perti_gelo],
+    [perti_cola_suave,perti_rum_fraco,perti_gelo])
+    
+    if not any(perti_cola_fraco,perti_cola_suave,perti_cola_forte):
+        falta_cola = True
+        
+
+if not any(perti_rum_fraco, perti_rum_suave, perti_rum_forte):
+    falta_rum = True
+    
+
+if perti_gelo == 0:
+    falta_gelo = True   
+    
+print(
+f"""
+PERTINÊNCIAS
+
+Pertinência de Rum Fraco {perti_rum_fraco}
+
+Pertinência de Rum Suave {perti_rum_suave}
+
+Pertinência de Rum Forte {perti_rum_forte}
+
+Pertinência de Refrigerante Fraco {perti_cola_fraco}
+
+Pertinência de Refrigerante Suave {perti_cola_fraco}
+
+Pertinência de Refrigerante Forte {perti_cola_fraco}
+
+Pertinência de Gelo {perti_gelo}
+
+DEFUZZY
+
+Defuzzy Fraco {defuzzy_fraco}
+
+Defuzzy Suave {defuzzy_suave}
+
+Defuzzy Forte {defuzzy_forte}
+
+
+"""   
+)
+
+if any([falta_cola,falta_gelo,falta_rum]):
+    print("O Drink feito nao e o Cuba Libre por que\n")
+    
+    if falta_cola:
+        print("Refrigerante do tipo cola nao se enquadrou como fraco, suave ou forte\n")
+        
+    if falta_rum:
+        print("Rum nao se enquadrou como fraco, suave ou forte\n")
+        
+    if falta_gelo:
+        print("Nao tem gelo no drink\n")
+    
+    print("portanto o valor cobrado sera de R$ 30,00")
+    
+else:
+    valorMaximo = max([defuzzy_forte,defuzzy_fraco,defuzzy_suave])
+    
+    if valorMaximo == defuzzy_fraco:
+        print("O paladar determinante é fraco, deverá ser cobrado R$ 15,00.")
+    
+    if valorMaximo == defuzzy_suave:
+        print("O paladar determinante é suave, deverá ser cobrado R$ 20,00.")
+    
+    if valorMaximo == defuzzy_forte:
+        print("O paladar determinante é forte, deverá ser cobrado R$ 25,00")
+    
+    
+
+
 
 
